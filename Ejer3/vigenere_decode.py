868c26d08203b71391feed1c9259eff97dfbcdf4
V1ctor2911 <omar29112004@gmail.com>
2026-04-23 14:21:55 +0200

    primreras vainas vigenere

---
+++ Ejer3/vigenere_decode.py
@@ -0,0 +1,25 @@
+limite = 52
+alfabeto = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+vigenere = []
+
+print(alfabeto)
+print(vigenere)
+for i in range(0,limite):
+    vigenere.append(alfabeto.copy())
+    alfabeto.append(alfabeto.pop(0))
+
+clave = input()
+mensaje = input()
+
+claves = []
+
+n = len(mensaje)//len(clave) 
+if (len(mensaje) % len(clave)) != 0:
+    for i in range(n):
+        for j in range(len(clave)):
+            claves.append(clave[j])
+    for i in range(len(mensaje) % len(clave)):
+        claves.append(clave[i])
+else:
+    print("si")
+print(claves)
