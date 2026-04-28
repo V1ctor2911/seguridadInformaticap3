d709e1b6c8c1bc3524dbd5cb0b97fb019c8b775b
V1ctor2911 <omar29112004@gmail.com>
2026-04-28 12:01:03 +0200

    descifrado vigenere

--- Ejer3/vigenere_decode.py
+++ Ejer3/vigenere_decode.py
@@ -1,6 +1,8 @@
 limite = 52
 alfabeto = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 vigenere = []
+    
+
 
 print(alfabeto)
 print(vigenere)
@@ -8,8 +10,8 @@ for i in range(0,limite):
     vigenere.append(alfabeto.copy())
     alfabeto.append(alfabeto.pop(0))
 
-clave = input().strip()
-mensaje = input().strip()
+clave = input("introduce la clave: ").strip()
+mensaje = input("introduzca el mensaje cifrado:  ").strip()
 
 claves = []
 
@@ -21,5 +23,24 @@ if (len(mensaje) % len(clave)) != 0:
     for i in range(len(mensaje) % len(clave)):
         claves.append(clave[i])
 else:
-    print("si")
-print(claves)
+    for i in range(n):
+        for j in range(len(clave)):
+            claves.append(clave[j])
+
+
+longClave = len(claves)
+def buscar(x, y):
+    i = 0
+    while vigenere[i][0] != x:
+        i += 1
+    lista = vigenere[i]
+    j = 0
+    while y != lista[j]:
+        j += 1
+    return alfabeto[j]
+res = []
+for i in range(0, longClave):
+    res.append(buscar(claves[i], mensaje[i]))
+
+resultado = "".join(res)
+print("El mensaje descifrado es: " + resultado)  
